ac08941c1cd3661fad7bdf840c370413cd668f82
Dylan Green <cobaltgoldcsprinks@gmail.com>
2023-12-13 16:48:19 -0700

    Added some simple tests for the tokenizer

--- src/tokenizer.rs
+++ src/tokenizer.rs
@@ -1,5 +1,6 @@
 use std::iter::Peekable;
 
+#[derive(PartialEq, Debug)]
 pub enum Token {
     Start(String),
     End,
@@ -46,4 +47,83 @@ fn consume_to_char(
         return_string.push(chars.next()?);
     }
     Some(return_string)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::{tokenize_string, Token};
+
+    #[test]
+    fn tokenizer_recognizes_whitespace() {
+        let input = " \n \n".to_owned();
+        let tokens = tokenize_string(input);
+
+        assert_eq!(
+            vec![
+                Token::Whitespace(' '),
+                Token::Whitespace('\n'),
+                Token::Whitespace(' '),
+                Token::Whitespace('\n')
+            ],
+            tokens,
+            "Tokens should be properly handled"
+        );
+
+        match tokens[0] {
+            Token::Whitespace(char) => assert_eq!(
+                char, ' ',
+                "First token should properly hold correct whitespace character."
+            ),
+            _ => assert!(false, "Incorrect type"),
+        }
+
+        match tokens[1] {
+            Token::Whitespace(char) => assert_eq!(
+                char, '\n',
+                "First token should properly hold correct whitespace character."
+            ),
+            _ => assert!(false, "Incorrect type"),
+        }
+
+        match tokens[2] {
+            Token::Whitespace(char) => assert_eq!(
+                char, ' ',
+                "First token should properly hold correct whitespace character."
+            ),
+            _ => assert!(false, "Incorrect type"),
+        }
+
+        match tokens[3] {
+            Token::Whitespace(char) => assert_eq!(
+                char, '\n',
+                "First token should properly hold correct whitespace character."
+            ),
+            _ => assert!(false, "Incorrect type"),
+        }
+    }
+
+    #[test]
+    fn tokenizer_handles_simple_text() {
+        let input = "!normal This is a test$".to_owned();
+        let tokens = tokenize_string(input);
+
+        for token in &tokens {
+            match token {
+                Token::Start(value) => assert_eq!("normal", value),
+                Token::Text(text) => assert_eq!("This is a test", text),
+                Token::Whitespace(value) => assert_eq!(&' ', value),
+                _ => (),
+            }
+        }
+
+        assert_eq!(
+            vec![
+                Token::Start("normal".to_string()),
+                Token::Whitespace(' '),
+                Token::Text("This is a test".to_string()),
+                Token::End
+            ],
+            tokens
+        );
+    }
 }
